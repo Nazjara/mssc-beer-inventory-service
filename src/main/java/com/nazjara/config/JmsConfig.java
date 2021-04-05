f8dd6f767217f7ceb59a507321e8be3ff3b3473a
Nazar Lipkovskyy <nazar.lipkovskyy@wds.co>
2021-04-05 16:57:20 +0300

    Refactoring

--- src/main/java/com/nazjara/config/JmsConfig.java
+++ src/main/java/com/nazjara/config/JmsConfig.java
@@ -1,4 +1,4 @@
-package guru.sfg.beer.inventory.service.config;
+package com.nazjara.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
